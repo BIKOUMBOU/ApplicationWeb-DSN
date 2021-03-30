3ad2d31982b4eb0b846dc702eca9225befd26542
BIKOUMBOU <66450547+BIKOUMBOU@users.noreply.github.com>
2021-03-30 08:42:37 +0200

    Version de ce 30/03/2021

---
+++ src/app/site.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {environment } from 'src/environments/environment';
+import { Site } from './site';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class SiteService{
+    
+    private apiServerUrl=environment.apiBaseUrl;
+    constructor(private http: HttpClient) {}
+
+    public getSites():Observable<Site[]>{
+        return this.http.get<Site[]>('${this.apiServerUrl}/sites');
+    }
+    public addSites(sites:Site[]):Observable<Site[]>{
+        return this.http.post<Site[]>('${this.apiServerUrl}/addSites',sites);
+    }
+
+    public addSite(site:Site[]):Observable<Site>{
+        return this.http.post<Site>('${this.apiServerUrl}/addSite',site);
+    }
+
+    public updateSite(site:Site):Observable<Site>{
+        return this.http.put<Site>('${this.apiServerUrl}/updateSite',site);
+    }
+   
+    public deleteSite(site:Site):Observable<Site>{
+        return this.http.delete<Site>('${this.apiServerUrl}/deleteSite');
+    }
+
+
+}
---
+++ src/app/site.ts
@@ -0,0 +1,26 @@
+export interface Site{
+
+    id: number;
+    priorite: String;
+    nom: String;
+    siren: number;
+    statusinstal: String;
+    hebergement: String;
+    acces: String;
+    resprojet: String;
+    consuldsn: String;
+    versagirh: String;
+    versprodige: String;
+    ifp: String;
+    mirth: String;
+    webserpH7: String;
+    configagirh: String;
+    configprodige: String;
+    nomencdesabsences: String;
+    nomencdesemplois: String;
+    nomencdestatuts: String;
+
+}
+
+
+ 
