f5291caec6e244658448e38bbeb0bbe466edb649
BIKOUMBOU <66450547+BIKOUMBOU@users.noreply.github.com>
2021-03-30 08:31:14 +0200

    Version de ce 30/03/2021

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Site } from'./site';
+import { SiteService } from './site.service';
+import { HttpErrorResponse } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'applicationWeb';
+export class AppComponent implements OnInit {
+  public sites: Site[];
+
+  constructor(private siteService: SiteService) {}
+
+  ngOnInit(){
+    this.getSites();
+  }
+
+  public getSites(): void{
+    this.siteService.getSites().subscribe(
+      (response: Site[]) => {
+        this.sites = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  title = 'Gestion Suivi Installation Dsn';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { SiteService } from './site.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [SiteService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
